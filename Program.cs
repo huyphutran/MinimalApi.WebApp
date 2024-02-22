95fec658e721e7124906f11c06d1ccb589c7d059
Phu Tran <tranhuyphuhp75@gmail.com>
2024-02-23 00:53:32 +0700

    Initial Inmemory Db and Sqlite

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>{
            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
 
 });
 
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.EditPizza(pizza));
-app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+app.MapGet("/pizzas",async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizzas/{id}", async (PizzaDb db,int id) => await db.Pizzas.FirstOrDefaultAsync(p => p.Id == id));
+app.MapPost("/pizzas", async (PizzaDb db, Pizza pizza) => {
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}",pizza);
+});
+app.MapPut("/pizzas", async (PizzaDb db,Pizza updatePizza, int id) =>{
+    var p = await db.Pizzas.FindAsync(id);
+    if (p is null) return Results.NotFound();
+    p.Name = updatePizza.Name;
+    p.Description = updatePizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+} );
+app.MapDelete("/pizzas/{id}", async (PizzaDb db,int id) => {
+        var p = await db.Pizzas.FindAsync(id);
+        if(p is null) return Results.NotFound();
+         db.Pizzas.Remove(p);
+         await db.SaveChangesAsync();
+         return Results.Ok();
+});
 
 
 app.UseSwagger();
--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d544bcdb03ad21beb5d2d9f133d37c5bc339c65c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
